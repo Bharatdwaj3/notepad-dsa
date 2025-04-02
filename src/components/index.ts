fc256fd7013f2dd7457f6364f78eb377ec94cd56
Bharatdwaj3 <badrikedar.jaydutt.456@gmail.com>
2025-04-02 18:30:41 +0530

    added hero sction in export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Footer from './Footer'
 import Header from './Header'
 import Navbar from './Navbar'
+import Hero from './Hero'
 
-export {Footer, Header, Navbar}
+export {Footer, Header, Navbar, Hero}
