b3dc6fa40c5b6bb9ddddd98572bd23369ae057b7
Bharatdwaj3 <badrikedar.jaydutt.456@gmail.com>
2025-04-06 19:30:24 +0530

    added some more styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import {Ctype} from './types'
 const Footer = ({name, type}:Ctype) => {
   return (
     <>
-        <div className='p-16 h-96 w-screen bg-red-300 absolute inset-x-0 bottom-0'>
+        <div className='p-16 h-96 w-screen bg-red-300 absolute bottom-0'>
           <h1>{name}</h1>
           <h3>{type}</h3>
         </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,9 +3,18 @@ import {Ctype} from './types'
 const Hero = ({name,type}:Ctype) => {
   return (
     <>
-       <div className='p-16 h-[1100px] w-screen absolute inset-x-0 top-115 bg-green-300'>
+       <div className='p-16 h-[1900px] w-screen absolute top-115 bg-green-300'>
           <h1>{name}</h1>
           <h3>{type}</h3>
+            <div className='mt-32 p-8 h-96 w-[1360px] rounded-xl bg-amber-200'>
+
+            </div>
+            <div className='grid grid-cols-2 grid-rows-2 gap-6 p-8 h-[800px] w-[1360px] '>
+                <div className='p-8 bg-amber-300 rounded-2xl'>AGrid</div>
+                <div className='p-8 bg-amber-300 rounded-2xl'>BGrid</div>
+                <div className='p-8 bg-amber-300 rounded-2xl'>CGrid</div>
+                <div className='p-8 bg-amber-300 rounded-2xl'>DGrid</div>
+            </div>
         </div>
     </>
   )
